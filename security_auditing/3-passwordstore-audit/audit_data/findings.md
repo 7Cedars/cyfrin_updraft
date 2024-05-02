7c1e96314a87198eca4e1b9e7c3097b273eafc57
Seven Cedars <Cedars7@proton.me>
2024-05-02 08:48:46 +0100

    end of section 3

--- security_auditing/3-passwordstore-audit/audit_data/findings.md
+++ security_auditing/3-passwordstore-audit/audit_data/findings.md
@@ -1,8 +1,4 @@
-1. Convince the protocol that it is an issue. 
-2. Argue for severity of issue. 
-3. Explain how to fix the issue. 
-
-### [S-#] Storing password on chain makes it visible to anyone, and no longer private. 
+### [H-1] Storing password on chain makes it visible to anyone, and no longer private. 
 
 **Description:** All on-chain data is visible to anyone, as it can be read directly from blockchain. The `PasswordStore::s_password` variable is intended to be private and only callable through the `PasswordStore::getPassword` function. Instead, s_passowrd is stored on_chain and hence publicly accesible. 
 
@@ -36,3 +32,65 @@ cast parse-bytes32-string 0x6d7950617373776f726400000000000000000000000000000000
 Output: `myPassword`
 
 **Recommended Mitigation:** 
+The overall architecture needs to be rethought. Storage of any password (and its encoding) need to happen off-chain. 
+
+
+### [H-2] `PasswordStore::setPassword` has no access control, meaning a non-owner can change the password. 
+
+**Description:** 
+The `PasswordStore::setPassword` lacks any access control. It means that any address can call the function and change the password. This is contrary to the natspec that states that `This function allows only the owner to set a new password`. 
+
+```javascript
+  function setPassword(string memory newPassword) external {
+  @>   //@audit no access control in previous line. 
+      s_password = newPassword;
+      emit SetNetPassword();
+  }
+```
+
+**Impact:** Anyone can set the password of the contract, severely breaking intended contract functionality. 
+
+**Proof of Concept:** Add the following to the `PasswordStore.t.sol` test file. 
+
+<details>
+<summary> expand code </summary>
+
+```javascript 
+  function test_anyone_can_set_password(address randomAddress) public { 
+      vm.assume(randomAddress != owner); 
+      vm.prank(randomAddress);
+      string memory expectedPassword = "myNewPassword"; 
+
+      passwordStore.setPassword(expectedPassword); 
+
+      vm.prank(owner); 
+      string memory actualPassword = passwordStore.getPassword(); 
+      assertEq(actualPassword, expectedPassword); 
+  }
+```
+</details>
+
+
+**Recommended Mitigation:** Add an access control conditional to the `setPassword` function. 
+
+<details>
+<summary> expand code </summary>
+
+```javascript 
+  if (msg.sender != s_owner) {
+    revert PasswordStore_NotOwner(); 
+  }
+```
+</details>
+
+### [I-1] The `PasswordStore::getPassword` natspec indicates there is a param that does not exist, causing the natspec to be incorrect.
+
+**Description:** 
+The `PasswordStore::getPassword` function signature is `getPassword()` while the natspec indicates it should be `getPassword(string)`. 
+
+**Impact:** Natspec is incorrect. 
+
+**Recommended Mitigation:** Remove incorrect natspec line. 
+```diff 
+- @param newPassword The new password to set.
+```
--- security_auditing/3-passwordstore-audit/test/PasswordStore.t.sol
+++ security_auditing/3-passwordstore-audit/test/PasswordStore.t.sol
@@ -1,47 +1,45 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18; //? correct compiler version? 
+pragma solidity 0.8.18;
 
-/*
- * @author not-so-secure-dev
- * @title PasswordStore
- * @notice This contract allows you to store a private password that others won't be able to see. 
- * You can update your password at any time.
- */
-contract PasswordStore {
-    error PasswordStore__NotOwner();
+import {Test, console} from "forge-std/Test.sol";
+import {PasswordStore} from "../src/PasswordStore.sol";
+import {DeployPasswordStore} from "../script/DeployPasswordStore.s.sol";
 
-    /* state vars */ 
-    address private s_owner;
-    //@audit all data is on-chain - not private! s_password is not actually private. Anyone can actually read this.  
-    string private s_password;
+contract PasswordStoreTest is Test {
+    PasswordStore public passwordStore;
+    DeployPasswordStore public deployer;
+    address public owner;
 
-    /* events */ 
-    event SetNetPassword();
-
-    /* constructor */
-    constructor() {
-        s_owner = msg.sender;
+    function setUp() public {
+        deployer = new DeployPasswordStore();
+        passwordStore = deployer.run();
+        owner = msg.sender;
     }
 
-    /*
-     * @notice This function allows only the owner to set a new password.
-     * @param newPassword The new password to set.
-     */
-    // @audit non-owner can set password - missing access control. 
-    function setPassword(string memory newPassword) external {
-        s_password = newPassword;
-        emit SetNetPassword();
+    function test_owner_can_set_password() public {
+        vm.startPrank(owner);
+        string memory expectedPassword = "myNewPassword";
+        passwordStore.setPassword(expectedPassword);
+        string memory actualPassword = passwordStore.getPassword();
+        assertEq(actualPassword, expectedPassword);
     }
 
-    /*
-     * @notice This allows only the owner to retrieve the password.
-     // @audit no param in function
-     * @param newPassword The new password to set.
-     */
-    function getPassword() external view returns (string memory) {
-        if (msg.sender != s_owner) {
-            revert PasswordStore__NotOwner();
-        }
-        return s_password;
+    function test_non_owner_reading_password_reverts() public {
+        vm.startPrank(address(1));
+
+        vm.expectRevert(PasswordStore.PasswordStore__NotOwner.selector);
+        passwordStore.getPassword();
+    }
+
+    function test_anyone_can_set_password(address randomAddress) public { 
+        vm.assume(randomAddress != owner); 
+        vm.prank(randomAddress);
+        string memory expectedPassword = "myNewPassword"; 
+
+        passwordStore.setPassword(expectedPassword); 
+
+        vm.prank(owner); 
+        string memory actualPassword = passwordStore.getPassword(); 
+        assertEq(actualPassword, expectedPassword); 
     }
 }
